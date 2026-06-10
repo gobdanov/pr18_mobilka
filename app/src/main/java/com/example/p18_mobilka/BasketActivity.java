5cb3786e459c35abb72e856169ea32cf4d390bc3
вадим благодетель <gobdanov@inbox.ru>
2026-06-10 11:42:12 +0500

    разработанный интерфейс для корзины. богданов

---
+++ app/src/main/java/com/example/p18_mobilka/BasketActivity.java
@@ -0,0 +1,17 @@
+package com.example.p18_mobilka;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BasketActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
