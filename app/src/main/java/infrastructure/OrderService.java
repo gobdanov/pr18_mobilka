c7dfc017e02da5e92e6a89d650b618e69418a833
вадим благодетель <gobdanov@inbox.ru>
2026-06-10 18:01:41 +0500

    разработанный сервис заказов. богданов

---
+++ app/src/main/java/infrastructure/OrderService.java
@@ -0,0 +1,96 @@
+package infrastructure;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.p18_mobilka.BasketActivity;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import datas.orders.OrderGet;
+import domains.NotifyManager;
+import domains.callbacks.MyResponseCallback;
+import domains.models.Order;
+
+public class OrderService  extends Service {
+    public Integer id;
+    String TAG = "ORDER SERVICE";
+    Integer Interval = 30 *1000;
+    Handler handler = new Handler();
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    NotifyManager notifyManager;
+
+    Runnable CheckStatusRunnable = new Runnable() {
+        @Override
+        public void run() {
+            OrderGet ResponseOrderGet = new OrderGet(
+                    id,
+                    BasketActivity.TOKEN,
+                    new MyResponseCallback() {
+                        @Override
+                        public void onCompile(String result) {
+                            Order OrderData = new GsonBuilder().create().fromJson(result, Order.class);
+                            if(OrderData.status == 1){
+                                Integer AllSum = 0;
+                                for (int i = 0; i<OrderData.products.size(); i++){
+                                    AllSum += OrderData.products.get(i).product.price * OrderData.products.get(i).count;
+                                }
+
+                                notifyManager.SendNotify(
+                                        "ваш заказ на сумму: " + AllSum +
+                                        "состоящий из " + OrderData.products.size() +
+                                        "товаров, успешно доставлен!!!");
+                            }
+                            else{
+                                handler.postDelayed(CheckStatusRunnable, Interval);
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+                            Log.e(TAG, error)
+                        }
+                    }
+            );
+            ResponseOrderGet.execute();
+        }
+    };
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        notifyManager = new NotifyManager(this);
+        Log.d(TAG, "service created");
+    }
+
+    @Override
+    public int onStartCommand(Intent Intent, int flags, int startId){
+        handler.removeCallbacks(CheckStatusRunnable);
+        handler.post(CheckStatusRunnable);
+        if(Intent != null && Intent.hasExtra("id")){
+            id = Intent.getIntExtra("id", -1);
+        }
+
+        return  START_STICKY;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        handler.removeCallbacks(CheckStatusRunnable);
+        executorService.shutdown();
+        Log.d(TAG, "Service destroyed");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent){
+        return  null;
+    }
+}
